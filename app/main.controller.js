43abff070320faa3343bfc6a8b02d9e8f7e7a9a8
Carolina Penagos <carolinapenagos@henao.local>
2015-12-15 08:35:02 -0500

    map function add

--- app/main.controller.js
+++ app/main.controller.js
@@ -1,21 +1,38 @@
 angular.module('weatherApp')
-    .controller('MainController', function(weatherService) {
-        
-        var self = this;
+.controller('MainController', function(weatherService) {
+      
+  var self = this;
+  self.city = '';
+  self.show = false;
+  
+  self.currentWeather = {};
 
-        self.city = '';
-        self.currentWeather = {};
+  self.getWeather = function getWeather(city){
+    var promise = weatherService.getWeather(city)
+    promise.then(function (weatherData) {
+      self.currentWeather = weatherData;
+      setMap();
+      self.show = true;
+    },
+    function (reason) {
         
-        self.getWeather = function getWeather(city){
-            var promise = weatherService.getWeather(city)
-            promise.then(function (weatherData) {
-                self.currentWeather = weatherData;
-            },
-            function (reason) {
-                
-            });
-        }
+    });
+  }
 
+  function setMap() {
+    var mapOptions = {
+      zoom: 4,
+      center: new google.maps.LatLng(self.currentWeather.coord.lat, self.currentWeather.coord.lon),
+      mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+    self.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  }
+
+  self.openInfoWindow = function(e, selectedMarker){
+    e.preventDefault();
+    google.maps.event.trigger(selectedMarker, 'click');
+  }
 });     
 
 
